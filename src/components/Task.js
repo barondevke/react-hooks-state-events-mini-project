b9d7513daa982c5226f6ab6249b9ef32b4def613
Steve Baron <ndabasteve1@gmail.com>
2023-07-20 21:43:13 +0300

    Complete

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Task() {
+function Task(props) {
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{props.category}</div>
+      <div className="text">{props.text}</div>
+      <button type="button" onClick={props.handleClick} className="delete">X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from './Task'
 
-function TaskList() {
+
+function TaskList(props) {
+  const [tasks, setTasks] = useState(props.tasks)
+
+  function handleDelete(taskId) {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task, index) => {
+        return (
+          <Task key={index} text={task.text} handleClick={event => handleDelete(event, index)} />
+
+        )
+
+
+      })}
+
     </div>
-  );
+  )
+
 }
 
 export default TaskList;
